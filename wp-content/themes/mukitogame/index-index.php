13fdabc157cdb0bbd29a3ebb7bf58a3e63a251a6
Anibal C <detops.tk@gmail.com>
2016-01-17 13:13:57 -0500

    up css and index

--- wp-content/themes/mukitogame/index-index.php
+++ wp-content/themes/mukitogame/index-index.php
@@ -23,8 +23,8 @@
 			<div class="row">
 				<div class="col-md-4 col-xs-4 text-center x-btn-1">
 					<div class="_">
-						<a href="play.html?player=1&t=m"><img src="<?php echo get_template_directory_uri() ?>/public/image/player1.png"></a>
-                                            </div>
+						<a href="play.html?player=1&t=m"><img class="img-responsive" src="<?php echo get_template_directory_uri() ?>/public/image/player1.png"></a>
+                    </div>
 					<div class="_">
 						<a class="font-1 text-shadow-black" href="play.html?player=1&t=m">player 1</a>
 					</div>
@@ -32,7 +32,7 @@
 
 				<div class="col-md-4 col-xs-4 text-center x-btn-1">
 					<div class="_">
-						<a href="play.html?player=2&t=m"><img src="<?php echo get_template_directory_uri() ?>/public/image/player2.png"></a>
+						<a href="play.html?player=2&t=m"><img class="img-responsive" src="<?php echo get_template_directory_uri() ?>/public/image/player2.png"></a>
 					</div>
 					<div class="_">
 						<a class="font-1 text-shadow-black" href="play.html?player=2&t=m">player 2</a>
@@ -41,7 +41,7 @@
 
 				<div class="col-md-4 col-xs-4 text-center x-btn-1">
 					<div class="_">
-						<a href="play.html?player=3&t=m"><img src="<?php echo get_template_directory_uri() ?>/public/image/player3.png"></a>
+						<a href="play.html?player=3&t=m"><img class="img-responsive" src="<?php echo get_template_directory_uri() ?>/public/image/player3.png"></a>
 					</div>
 					<div class="_">
 						<a class="font-1 text-shadow-black" href="play.html?player=3&t=m">player 3</a>
